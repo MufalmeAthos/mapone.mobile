558dd2dcaf69d9feb5879eb2b390b9d434abcba4
Athola Bahizire <atholabahizire@gmail.com>
2022-04-05 23:34:01 +0200

    Call bdy widget on main

--- lib/Body.dart
+++ lib/Body.dart
@@ -1,20 +1,58 @@
 import 'package:flutter/material.dart';
 
- class Body extends StatelessWidget {
-   const Body({Key? key}) : super(key: key);
+ class Body extends StatefulWidget {
+   final title;
+   const Body({Key? key, this.title}) : super(key: key);
 
-   @override
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
    Widget build(BuildContext context) {
-     return SingleChildScrollView(
-       child: Container(
-         child: Column(
-           children: [
-             Text("Here is the new page"),
+     return Scaffold(
+       appBar: AppBar(title: Text("Home"),),
+       body: SingleChildScrollView(
+         child: Container(
+           child: Column(
+             children: [
+               Text("Here is the new page"),
+               Center(
 
-           ],
+                 child: Column(
+
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   children: <Widget>[
+                     const Text(
+                       'You have pushed the button this many times:',
+                     ),
+                     Text(
+                       '$_counter',
+                       style: Theme.of(context).textTheme.headline4,
+                     ),
+                   ],
+                 ),
+               ),
+             ],
+           ),
+
          ),
-
+       ),
+       floatingActionButton: FloatingActionButton(
+         onPressed: _incrementCounter,
+         tooltip: 'Incrementation',
+         child: const Icon(Icons.add),
        ),
      );
    }
- }
+}
